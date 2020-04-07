70f4dbf3a50d8c08d853a7c036878f2f6f4899a4
Anthony Djoewanda <16738612+tonychuaco@users.noreply.github.com>
2020-04-07 15:53:00 -0400

    Updates

---
+++ precache-manifest.606b865c41d71e6c3ca2f82041446e43.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a39347621ccbcc7d2c58f64d9e39ef89",
+    "url": "/react-js-building-interface/index.html"
+  },
+  {
+    "revision": "a07ae5e401fbe309fca2",
+    "url": "/react-js-building-interface/static/css/2.89f067aa.chunk.css"
+  },
+  {
+    "revision": "a19639578c2d76f495f4",
+    "url": "/react-js-building-interface/static/css/main.075a71a4.chunk.css"
+  },
+  {
+    "revision": "a07ae5e401fbe309fca2",
+    "url": "/react-js-building-interface/static/js/2.c6fa7700.chunk.js"
+  },
+  {
+    "revision": "e386ada3ad03a9606684b1f649731b40",
+    "url": "/react-js-building-interface/static/js/2.c6fa7700.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a19639578c2d76f495f4",
+    "url": "/react-js-building-interface/static/js/main.6e2eba0f.chunk.js"
+  },
+  {
+    "revision": "85991d83572d89b72756",
+    "url": "/react-js-building-interface/static/js/runtime-main.104ad62c.js"
+  }
+]);
